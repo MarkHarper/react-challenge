9601e2516421623d975c6900c2f4f7924c80ae4e
Mark Harper <markharper20@gmail.com>
2016-08-26 17:20:42 -0400

    additional test for bringing patient data together

--- app/helpers/utils.spec.js
+++ app/helpers/utils.spec.js
@@ -1,6 +1,31 @@
 import expect from 'expect'
 import { describe, it } from 'mocha'
-import { formatPatientData, age } from './utils'
+import { formatPatientData, age, deliverPatientData } from './utils'
+
+const examplePatientArr = [{
+  'name': {
+    'first': 'Harry',
+    'last': 'Carroll',
+  },
+  'mrn': '56789',
+  'dob': '06/12/1964',
+  'sex': 'Male',
+  'treatment_site': 'Prostate',
+  'tumor_size_cm': 0.5,
+  'histology': 'Carcinoma',
+  'weight': 212.2,
+}]
+
+const processedExample = [{
+  'name': 'Harry Carroll',
+  'mrn': '56789',
+  'dob': '06/12/1964',
+  'demographics': '52 y.o Male',
+  'treatment_site': 'Prostate',
+  'tumor_size': 0.5,
+  'histology': 'Carcinoma',
+  'weight': 212.2,
+}]
 
 describe('age', () => {
   it('should calculate the correct age, given a date string', () => {
@@ -15,30 +40,18 @@ describe('age', () => {
 
 describe('formatPatientData', () => {
   it('reformat a patient\'s data to match the desired properties', () => {
-    let case1 = {
-      'name': {
-        'first': 'Harry',
-        'last': 'Carroll',
-      },
-      'mrn': '56789',
-      'dob': '06/12/1964',
-      'sex': 'Male',
-      'treatment_site': 'Prostate',
-      'tumor_size_cm': 0.5,
-      'histology': 'Carcinoma',
-      'weight': 212.2,
-    }
-    let answer1 = [{
-      'name': 'Harry Carroll',
-      'mrn': '56789',
-      'dob': '06/12/1964',
-      'demographics': '52 y.o Male',
-      'treatment_site': 'Prostate',
-      'tumor_size': 0.5,
-      'histology': 'Carcinoma',
-      'weight': 212.2,
-    }]
+    let case1 = examplePatientArr[0]
+    let answer1 = processedExample
 
     expect(formatPatientData(case1, [])).toEqual(answer1)
+  })
+})
+
+describe('deliverPatientData', () => {
+  it('reformat an array of patient data', () => {
+    let case1 = examplePatientArr
+    let answer1 = processedExample
+
+    expect(deliverPatientData(case1, [])).toEqual(answer1)
   })
 })
